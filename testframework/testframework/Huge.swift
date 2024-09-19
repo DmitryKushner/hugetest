bd5b14f5769720f7f93829d479b025b25d02c8ca
DmitryKushner <38130380+DmitryKushner@users.noreply.github.com>
2024-09-19 14:14:34 +0300

    Test

---
+++ testframework/testframework/Huge.swift
@@ -0,0 +1,14 @@
+//
+//  Huge.swift
+//  testframework
+//
+//  Created by Kushner Dzmitry on 19.09.2024.
+//
+
+import Foundation
+
+@objc public final class Huge: NSObject {
+    @objc func printHuge() {
+        print("HUGE")
+    }
+}
